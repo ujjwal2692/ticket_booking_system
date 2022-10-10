c7a7f0922bc5e9f438b05bc46df2e6f5c3c978b6
Amnnnn16 <ag6835902@gmail.com>
2022-10-10 12:14:44 +0530

    booking function upd..

--- src/com/system/Booking.java
+++ src/com/system/Booking.java
@@ -1,20 +1,17 @@
 package com.system;
 import java.util.Date;
 
-public class Booking extends Status{
+public class Booking extends  Status{
 
 public void Book(){
     Date d = new Date();
-    if(status()){
+    if(status()==1){
         System.out.println("Ticket Booked at : "+d);
         n=n-1;
     }
     else{
         System.out.println("Unable to book");
 
-    }
-     
-    
-        
+    }    
     }
 }
