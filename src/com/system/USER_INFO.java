57550bc121cdd6d857cedff8fd83a0a511b3e3e5
Ujjwal Kumar Gupta <ujjwalgupta1.ujg@gmail.com>
2022-10-10 11:51:24 +0530

    USER INFORMATION

---
+++ src/com/system/USER_INFO.java
@@ -0,0 +1,18 @@
+package com.system;
+import java.util.*;
+public class USER_INFO{
+    public void user_information()
+    {
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Name of the customer");
+    String s1= sc.nextLine();
+    System.out.println("Enter the age :");
+   int age= sc.nextInt();
+    System.out.println("Enter the Aadhar Id :");
+  Long l1=  sc.nextLong();
+  sc.close();
+
+
+}
+
+}
