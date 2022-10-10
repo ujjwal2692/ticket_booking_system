dbf1054d5ca0086220bc1147483d28795b39a8fc
Aryansh <500093038@stu.upes.ac.in>
2022-10-10 12:23:09 +0530

    main function created

--- src/com/system/Main.java
+++ src/com/system/Main.java
@@ -1,6 +1,15 @@
 package com.system;
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        USER_INFO ob2 = new USER_INFO();
+        ob2.user_information();
+        Booking ob1 = new Booking();
+        ob1.Book();
+        Status ob = new Status();
+        ob.status();
+        Cancel ob3 = new Cancel();
+        ob3.cancel_ticket();
         
+
     }
 }
