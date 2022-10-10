f04d1b9b864b08860728dd15708d2cd7900aa366
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2022-10-10 12:09:22 +0530

    updating changes

--- src/com/system/Cancel.java
+++ src/com/system/Cancel.java
@@ -1,7 +1,12 @@
 package com.system;
 
-public class Cancel {
+public class Cancel extends Status {
     public void cancel_ticket(int ticket_del){
-        
+        if(ticket_del>super.n){
+            System.out.println("Please enter valid ticket delete number");
+        }else{
+            super.n+=ticket_del;
+            System.out.println("Tickets cancelled successfully");
+        }
     }
 }
